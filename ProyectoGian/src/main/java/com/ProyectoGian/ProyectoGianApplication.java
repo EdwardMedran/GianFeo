77886a652881a48e99eaeb93ed309574ad23c43c
Edward Medrano <edwardgino13@gmail.com>
2025-07-20 03:35:21 -0500

    Agregando proyecto Spring

---
+++ ProyectoGian/src/main/java/com/ProyectoGian/ProyectoGianApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoGian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGianApplication.class, args);
+	}
+
+}
