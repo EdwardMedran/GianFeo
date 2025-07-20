13d28e70ed20822d287e452b66526b4d0ef9ea58
GiantCc <gianch2000@gmail.com>
2025-07-20 02:44:20 -0500

    Sout 2

--- JavaBank/src/main/java/org/example/Main.java
+++ JavaBank/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo p chulapi");
+        System.out.println("Hola mundo p chulapi 2");
     }
 }
