0ce171b37cf70d78bbb2f83d408370c61935ed8d
GiantCc <gianch2000@gmail.com>
2025-07-20 02:52:42 -0500

    scanner

--- JavaBank/src/main/java/org/example/Main.java
+++ JavaBank/src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         System.out.println("Hola mundo p chulapi");
         System.out.println("Hola mundo p chulapi 2");
+        nombre = leer.next();
     }
 }
